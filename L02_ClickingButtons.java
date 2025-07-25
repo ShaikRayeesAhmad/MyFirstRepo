cd55fbe3ba8569e39ab467a1a089bba45201b4fd
ShaikRayeesAhmad <rayeesahmedme@gmail.com>
2025-07-25 12:48:09 +0530

    these are are tracked

---
+++ L02_ClickingButtons.java
@@ -0,0 +1,22 @@
+package Section05;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class L02_ClickingButtons {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		System.setProperty("webdriver.chrome.driver", "/Users/rayeesmacbookair/Documents/chromedriver");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://automationtesting.co.uk/buttons.html");
+		driver.findElement(By.cssSelector("button#btn_three")).click();
+		Thread.sleep(3000);
+		driver.close();
+		driver.quit();
+
+	}
+
+}
